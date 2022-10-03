ab5127420617c6d1a25eaff2cfe20369c7b94dc1
john-mirage <chaosdynamix@posteo.net>
2022-10-03 13:07:03 +0200

    fix: fix stat list responsive design

--- src/components/Stat/component.tsx
+++ src/components/Stat/component.tsx
@@ -10,8 +10,8 @@ interface Props {
 const Stat: FunctionComponent<Props> = ({ className, statName, statValue }) => {
   return (
     <li className={`stat ${className}`}>
+      <h2 className="stat__name">{statName}</h2>
       <p className="stat__value">{statValue}</p>
-      <p className="stat__name">{statName}</p>
     </li>
   );
 }
