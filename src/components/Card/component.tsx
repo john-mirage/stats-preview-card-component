d3a8b497d68704a097dd2f671bc6723c9ff8583a
john-mirage <chaosdynamix@posteo.net>
2022-10-03 11:47:39 +0200

    ref: use background image instead of picture

--- src/components/Card/component.tsx
+++ src/components/Card/component.tsx
@@ -1,9 +1,6 @@
 import { FunctionComponent } from "react";
-import "./style.css";
-
-import headerMobileImage from "@images/image-header-mobile.jpg";
-import headerDesktopImage from "@images/image-header-desktop.jpg";
 import Stat from "@components/Stat";
+import "./style.css";
 
 const stats = [
   {
@@ -23,13 +20,7 @@ const stats = [
 const Card: FunctionComponent = () => {
   return (
     <main className="card">
-      <div className="card__header">
-        <picture className="card__picture">
-          <source srcSet={headerDesktopImage} media="(min-width: 1158px)" />
-          <img className="card__image" src={headerMobileImage} alt="People in a meeting" draggable="false" />
-        </picture>
-        <div className="card__overlay"></div>
-      </div>
+      <div className="card__header"></div>
       <div className="card__body">
         <h1 className="card__title">Get <span className="card__titleEmphase">insights</span> that help your business grow.</h1>
         <p className="card__subtitle">Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</p>
